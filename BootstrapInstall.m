eee4cfc66c40f6f08d31a3d908fb147dafee7601
jkuczm <Jakub.Kuczmarski@gmail.com>
2014-11-11 01:35:43 +0100

    Version bump: 0.1.1

--- BootstrapInstall.m
+++ BootstrapInstall.m
@@ -5,7 +5,7 @@ Get["https://raw.githubusercontent.com/jkuczm/MathematicaBootstrapInstaller/v0.1
 
 BootstrapInstall[
 	"ClasslessObjects",
-	"https://github.com/jkuczm/MathematicaClasslessObjects/releases/download/v0.1.0/ClasslessObjects.zip",
+	"https://github.com/jkuczm/MathematicaClasslessObjects/releases/download/v0.1.1/ClasslessObjects.zip",
 	"AdditionalFailureMessage" ->
 		Sequence[
 			"You can ",
--- PacletInfo.m
+++ PacletInfo.m
@@ -1,10 +1,10 @@
 (* Paclet Info File *)
 
-(* created 2014/11/09*)
+(* created 2014/11/11*)
 
 Paclet[
     Name -> "ClasslessObjects",
-    Version -> "0.1.0",
+    Version -> "0.1.1",
     MathematicaVersion -> "6+",
     Description -> "Tools for creating classless objects with prototype based inheritance.",
     Creator -> "Jakub Kuczmarski",
